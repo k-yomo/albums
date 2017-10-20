feb01ab21d3312c00e5fd317c877266b0a2eecf2
yomoda07 <kanji.yy@gmail.com>
2017-10-19 22:19:56 -0700

    Modify the color of the header

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Header from './src/components/header';
 
 export default () => (
-  <Header />
+  <Header headerText={'Albums!'} />
 );
--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
+
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
+    backgroundColor: '#00BCD4',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
     paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    position: 'realative'
+    shadowOpacity: 0.3,
+    position: 'relative'
   },
   textStyle: {
-    fontSize: 20
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 };
 
