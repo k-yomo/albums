ba732e40a084703b11e62043308d15423d045b1c
yomoda07 <kanji.yy@gmail.com>
2017-10-20 00:58:18 -0700

    Create Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress }) => {
+  const { buttonStyle, textStyle } = styles;
   return (
-    <Text>Click me!</Text>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>Click me!</Text>
+    </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#00BCD4',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#00BCD4',
+    marginRight: 5,
+    marginLeft: 5
+  }
 };
 
 export default Button;
